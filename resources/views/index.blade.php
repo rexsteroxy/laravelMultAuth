7f414d0997f80d6fe37c19ae1b0d16caa3774273
rexsteroxy <rexsteroxy22@gmail.com>
2020-05-27 19:28:24 +0100

    worked on the backend

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,33 +1,31 @@
-
 @extends('layouts.app')
 <style type="text/css">
- .avatar{
-     border-radius:100%;
-     max-width : 200px;
- }
- </style>
+	.avatar {
+		border-radius: 100%;
+		max-width: 200px;
+	}
+</style>
 @section('content')
 
 <div class="container">
-				<div class="row">
-					<div class="col">
-						<div class="top_bar_content d-flex flex-row align-items-center justify-content-start">
-						@guest
-						<div class="top_bar_item"><a href="{{ route('register') }}">Register</a></div>
-						<div class="top_bar_item"><a href="{{ route('login') }}">Login</a></div>
-                        @else
-						<div class="top_bar_item" style="color:white;">{{ Auth::user()->name }} &nbsp &nbsp</div>
+	<div class="row">
+		<div class="col">
+			<div class="top_bar_content d-flex flex-row align-items-center justify-content-start">
+				@guest
 
-						<div class="jumbotron">
-<h2>Congratulations You are now logged In as a user</h2>
-</div>
-						@endguest
-						</div>
-<div class="jumbotron">
-<h2>Welcome to the home page</h2>
-</div>
-					</div>
+				@else
+				<div class="top_bar_item" style="color:white;">{{ Auth::user()->name }} &nbsp &nbsp</div>
+
+				<div class="jumbotron">
+					<h2>Congratulations You are now logged In as a user</h2>
 				</div>
+				@endguest
+			</div>
+			<div class="jumbotron">
+				<h2>Welcome to the home page</h2>
+			</div>
+		</div>
+	</div>
 </div>
 
 
