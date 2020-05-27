5f064812f53e55f957b2365df1cd126aa55d46e4
rexsteroxy <rexsteroxy22@gmail.com>
2020-05-27 17:35:51 +0100

    worked on the database

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,13 @@
+
+@extends('layouts.app')
+<style type="text/css">
+ .avatar{
+     border-radius:100%;
+     max-width : 200px;
+ }
+ </style>
+@section('content')
+
 <div class="container">
 				<div class="row">
 					<div class="col">
@@ -8,10 +18,17 @@
                         @else
 						<div class="top_bar_item" style="color:white;">{{ Auth::user()->name }} &nbsp &nbsp</div>
 
-						<h1>This is the home page</h1>
+						<div class="jumbotron">
+<h2>Congratulations You are now logged In as a user</h2>
+</div>
 						@endguest
 						</div>
-
+<div class="jumbotron">
+<h2>Welcome to the home page</h2>
+</div>
 					</div>
 				</div>
 </div>
+
+
+@endsection
